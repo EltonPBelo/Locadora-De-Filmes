da7a0b342ce632d8e314c425444ca36250c71695
Elton <eltonpbelo01@gmail.com>
2026-04-10 16:18:30 -0300

    up

--- Filmes/Filme.java
+++ Filmes/Filme.java
@@ -1,16 +1,43 @@
 package Filmes;
 
 public class Filme {
+
+    private String nome;
+    private String genero;
+    private int duracaoMinutos;
+    private int quantidade;
+    private double valorAluguel;
    
+    public Filme(String nome, String genero, int duracaoMinutos, int quantidade, double valorAluguel) {
+        this.nome = nome;
+        this.genero = genero;
+        this.duracaoMinutos = duracaoMinutos;
+        this.quantidade = quantidade;
+        this.valorAluguel = valorAluguel;
+    }
     public Filme(String nome, String genero, int duracaoMinutos) {
         this.nome = nome;
         this.genero = genero;
         this.duracaoMinutos = duracaoMinutos;
     }
-    private String nome;
-    private String genero;
-    private int duracaoMinutos;
-    
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+
+
     public String getNome() {
         return nome;
     }
